9e3d00e30f3437d19b64b3f71870ccd9a5bb0a3a
WilliamNunes905 <williamtrybe@gmail.com>
2024-09-13 15:21:36 -0300

    feat: Criado a classe Moto e extendendo de Veiculo e implementando os metodos da classe

---
+++ src/main/java/com/TryLog/ControleFrota/Caminhao.java
@@ -0,0 +1,39 @@
+package com.TryLog.ControleFrota;
+
+import java.util.ArrayList;
+
+public class Caminhao extends Veiculo implements Transportador {
+  private ArrayList<String> viajem;
+
+  public Caminhao(String placa, int anoFabricacao) {
+    super(placa, anoFabricacao);
+    this.setCombustivel("Diesel");
+    this.viajem = new ArrayList<>();
+  }
+
+  @Override
+  public void abastecer(String local, String motorista, double valor) {
+    this.getAbastecimento().add(String.format("Local: base, Motorista: %s, Valor: %.2f", motorista, valor));
+  }
+
+  @Override
+  public void transportar(String origem, String destino) {
+    this.getViajem().add(String.format("Veiculo de placa %s, vai transportar a carga de %s para %s.", this.getPlaca(), origem, destino));
+  }
+
+  public void listaViajem() {
+    System.out.println("##------ Lista de Viajem do Veiculo: " + getPlaca() + " ------##");
+    for (String viajem : this.viajem) {
+      System.out.println("|  " + viajem + "  |");
+    }
+    System.out.println("##----------------------####--####------------------------##");
+  }
+
+  public ArrayList<String> getViajem() {
+    return viajem;
+  }
+
+  public void setViajem(ArrayList<String> viajem) {
+    this.viajem = viajem;
+  }
+}
